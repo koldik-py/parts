5865dcca9b752a33ef1cc94f5d727df8d9e7ab17
ilya_belyshev <ikoldik@gmail.com>
2023-02-25 19:34:04 +0300

    Самое начало, смена направления +

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.sass';
+
+import Sections from './Sections/Sections';
+
+function App() {
+  return (
+    <div className="App df aistr">
+      <Sections />
+    </div>
+  );
+}
+
+export default App;
